27b5f04146263265bd1f6c3f25ab0f84d252efe8
Ivan Nikolić <niksy5@gmail.com>
2023-08-24 11:40:48 +0200

    Use pure ESM as default

--- index.js
+++ index.js
@@ -8,45 +8,53 @@ const messages = stylelint.utils.ruleMessages(ruleName, {
 	expected: 'Expected box-shadow inset values to be first in the value list.'
 });
 
-const plugin = stylelint.createPlugin(ruleName, (bool) => (cssRoot, result) => {
-	const validOptions = stylelint.utils.validateOptions(result, ruleName, {
-		actual: bool
-	});
-
-	if (!validOptions) {
-		return;
-	}
-
-	cssRoot.walkDecls('box-shadow', (decl) => {
-		/** @type {string[]} */
-		const list = [];
-
-		valueParser(decl.value).walk((node) => {
-			if (node.type === 'function') {
-				return false;
-			}
-			if (node.value === 'inset' || node.value === ',') {
-				list.push(node.value);
-			}
+/**
+ * @type  {stylelint.RuleBase}
+ */
+function ruleFunction(/** @type boolean*/ bool) {
+	return function (cssRoot, result) {
+		const validOptions = stylelint.utils.validateOptions(result, ruleName, {
+			actual: bool
 		});
 
-		const inputOrder = list.map((item) => {
-			if (item === ',') {
-				return 'normal';
-			}
-			return item;
-		});
-		const correctOrder = [...inputOrder].sort();
-
-		if (!isEqual(inputOrder, correctOrder)) {
-			stylelint.utils.report({
-				ruleName: ruleName,
-				result: result,
-				node: decl,
-				message: messages.expected
-			});
+		if (!validOptions) {
+			return;
 		}
-	});
-});
+
+		cssRoot.walkDecls('box-shadow', (decl) => {
+			/** @type {string[]} */
+			const list = [];
+
+			valueParser(decl.value).walk((node) => {
+				if (node.type === 'function') {
+					return false;
+				}
+				if (node.value === 'inset' || node.value === ',') {
+					list.push(node.value);
+				}
+			});
+
+			const inputOrder = list.map((item) => {
+				if (item === ',') {
+					return 'normal';
+				}
+				return item;
+			});
+			const correctOrder = [...inputOrder].sort();
+
+			if (!isEqual(inputOrder, correctOrder)) {
+				stylelint.utils.report({
+					ruleName: ruleName,
+					result: result,
+					node: decl,
+					message: messages.expected
+				});
+			}
+		});
+	};
+}
+
+// @ts-ignore
+const plugin = stylelint.createPlugin(ruleName, ruleFunction);
 
 export default { ...plugin, messages };
